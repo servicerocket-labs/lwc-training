4d8b975a1d6335e7b95b0021da2278262b27e408
Fadli Abdul Rahman <mohd.fadli@servicerocket.com>
2020-04-13 15:13:50 +0800

    Added working example.

--- src/main/default/lwc/firstComponent/firstComponent.js
+++ src/main/default/lwc/firstComponent/firstComponent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class FirstComponent extends LightningElement {}
+export default class FirstComponent extends LightningElement {
+	handleClick(event) {
+		const firstInput = this.template.querySelector('[data-id="first-input"]');
+		console.log("inputText:", firstInput.value);
+		alert(`You have insert "${firstInput.value}"`);
+	}
+}
