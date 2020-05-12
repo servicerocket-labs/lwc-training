4d02c9f1f3947b9d1b69ee630573ddcdc7b2ca0f
Fadli Abdul Rahman <mohd.fadli@servicerocket.com>
2020-05-12 11:52:35 +0800

    Study group session #2. LWC in VisualForce and decorators examples.

--- src/main/default/lwc/exampleComponent/exampleComponent.js
+++ src/main/default/lwc/exampleComponent/exampleComponent.js
@@ -1,8 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class ExampleComponent extends LightningElement {
-	inputValue;
+	@track name = {
+		"firstname": "",
+		"lastname": "",
+		"fullname":  ""
+	}
 
-	handleButtonClick = () => alert(`You have insert "${this.inputValue}"`);
-	handleInputChange = (event) => this.inputValue = event.target.value;
+	handleButtonClick = () => alert(`You have insert "${this.name.fullname}"`);
+	handleFirstnameChange = (event) => {
+		this.name.firstname = event.target.value;
+		this.name.fullname = this.name.firstname + " " + this.name.lastname
+	}
+	handleLastnameChange = (event) => {
+		this.name.lastname = event.target.value;
+		this.name.fullname = this.name.firstname + " " + this.name.lastname
+	}
 }
